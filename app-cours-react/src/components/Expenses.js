330e6214585cd8aab633d93e063782c05339f710
TristanIsitech <tristan.labaume@ecole-isitech.fr>
2023-01-10 10:03:20 +0100

    deuxieme exo

--- app-cours-react/src/components/Expenses.js
+++ app-cours-react/src/components/Expenses.js
@@ -3,21 +3,18 @@ import Card from './Card';
 import './Expenses.css';
 
 function Expenses(props) {
-    const showTab = () => {
-        return props.items.forEach(element => {
-            <ExpenseItem
-                title={element.title}
-                amount={element.amount}
-                date={element.date}
-            />
-        });
-    }
 
-  return (
-    <Card className="expenses">
-      
-    </Card>
-  );
+    return (
+        <Card className="expenses">
+            {props.items.forEach(element => {
+                return (<ExpenseItem
+                    title={element.title}
+                    amount={element.amount}
+                    date={element.date}
+                />)
+            })}
+        </Card>
+    );
 }
 
 export default Expenses;
